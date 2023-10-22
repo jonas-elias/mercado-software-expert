403ec1c40dc7e577d01ba736fee97158d3854d54
Jonas Elias <jonasdasilvaelias@gmail.com>
2023-10-22 17:45:25 +0000

    feat: injection dependencies interface controller

--- src/app/Controller/Controller.php
+++ src/app/Controller/Controller.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace Jonaselias\ExpertFramework\Controller;
+use ExpertFramework\Container\Container;
+use ExpertFramework\Http\Contract\RequestInterface;
+use ExpertFramework\Http\Contract\ResponseInterface;
 
 /**
  * class Controller
@@ -10,11 +13,23 @@ namespace Jonaselias\ExpertFramework\Controller;
 class Controller
 {
     /**
+     * @var ResponseInterface $response
+     */
+    public ResponseInterface $response;
+
+    /**
+     * @var RequestInterface $request
+     */
+    public RequestInterface $request;
+
+    /**
      * Method constructor
      *
      * @return void
      */
     public function __construct()
     {
+        $this->response = Container::get('ExpertFramework\Http\Response');
+        $this->request = Container::get('ExpertFramework\Http\Request');
     }
 }
