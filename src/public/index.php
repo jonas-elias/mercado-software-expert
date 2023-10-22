cf5e069b462d22b98335f45472df0da2c36f46a8
Jonas Elias <jonasdasilvaelias@gmail.com>
2023-10-22 17:42:35 +0000

    fix: index.php dispatch routes

---
+++ src/public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+use ExpertFramework\Container\Container;
+use ExpertFramework\Http\Router\Router;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+require_once __DIR__ . '/../../vendor/expert-framework/helpers/src/helpers.php';
+
+require_once __DIR__ . '/../routes/routes.php';
+
+$dispatcher = Container::get('ExpertFramework\Http\Router\Dispatcher');
+$request = Container::get('ExpertFramework\Http\Request');
+
+$dispatcher->handle($request, Router::getAllRoutes());
