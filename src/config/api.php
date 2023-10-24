a067846f36124644b676e6d504c5b00af96c1bd6
Jonas Elias <jonasdasilvaelias@gmail.com>
2023-10-24 02:21:11 +0000

    feat: messages api

---
+++ src/config/api.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'mensagens' => [
+        'sucesso' => [
+            'status' => 'sucesso',
+            'mensagem' => 'Operação bem-sucedida.'
+        ],
+        'erro_cliente' => [
+            'status' => 'erro',
+            'mensagem' => 'Requisição inválida.',
+        ],
+        'erro_interno' => [
+            'status' => 'erro',
+            'mensagem' => 'Erro interno do servidor.',
+        ]
+    ]
+];
