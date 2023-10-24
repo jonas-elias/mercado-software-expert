fe9f191977e65807c00854ead648a389a31e1d50
Jonas Elias <jonasdasilvaelias@gmail.com>
2023-10-24 02:22:55 +0000

    feat: routes produto

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -2,5 +2,23 @@
 
 use ExpertFramework\Http\Router\Router;
 
+/**
+ * @api api
+ */
 Router::get('/api', 'Api\Docs\SwaggerController@yaml');
 Router::get('/api/docs', 'Api\Docs\SwaggerController@doc');
+
+/**
+ * @api produto
+ */
+Router::post('/api/produto', 'Api\Produto\ProdutoController@insereProduto');
+Router::get('/api/produto', 'Api\Produto\ProdutoController@getProdutos');
+Router::put('/api/produto/{id}', 'Api\Produto\ProdutoController@atualizaProduto');
+Router::get('/api/produto/{id}', 'Api\Produto\ProdutoController@getProdutoById');
+Router::delete('/api/produto/{id}', 'Api\Produto\ProdutoController@deleteProdutoById');
+
+/**
+ * @api tipoProduto
+ */
+Router::post('/api/tipoProduto', 'Api\Produto\TipoProdutoController@insereTipoProduto');
+Router::get('/api/tipoProduto', 'Api\Produto\TipoProdutoController@getTipoProduto');
